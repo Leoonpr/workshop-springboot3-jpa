538b68b9f718901a2358fde6e05afebe9a9dc7cd
leonardo <rochaleo879@gmail.com>
2024-01-10 11:41:05 -0300

    OrderItem, many-to-many association with extra attributes

--- src/main/java/com/projeto/course/config/TestConfig.java
+++ src/main/java/com/projeto/course/config/TestConfig.java
@@ -10,10 +10,12 @@ import org.springframework.context.annotation.Profile;
 
 import com.projeto.course.entities.Category;
 import com.projeto.course.entities.Order;
+import com.projeto.course.entities.OrderItem;
 import com.projeto.course.entities.Product;
 import com.projeto.course.entities.User;
 import com.projeto.course.entities.enums.OrderStatus;
 import com.projeto.course.repositories.CategoryRepository;
+import com.projeto.course.repositories.OrderItemRepository;
 import com.projeto.course.repositories.OrderRepository;
 import com.projeto.course.repositories.ProductRepository;
 import com.projeto.course.repositories.UserRepository;
@@ -28,10 +30,12 @@ public class TestConfig implements CommandLineRunner {
 
 	@Autowired
 	private CategoryRepository categoryRepository;
-	
+
 	@Autowired
 	private ProductRepository productRepository;
 
+	@Autowired
+	private OrderItemRepository orderItemRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
@@ -50,20 +54,26 @@ public class TestConfig implements CommandLineRunner {
 		Product p2 = new Product(null, "Smart TV", "Nulla eu imperdiet purus. Maecenas ante.", 2190.0, "");
 		Product p3 = new Product(null, "Macbook Pro", "Nam eleifend maximus tortor, at mollis.", 1250.0, "");
 		Product p4 = new Product(null, "PC Gamer", "Donec aliquet odio ac rhoncus cursus.", 1200.0, "");
-		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, ""); 
-		
+		Product p5 = new Product(null, "Rails for Dummies", "Cras fringilla convallis sem vel faucibus.", 100.99, "");
+
 		userRepository.saveAll(Arrays.asList(u1, u2));
 		orderRepository.saveAll(Arrays.asList(o1, o2, o3));
 		categoryRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
-		
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+
 		p1.getCategories().add(cat2);
 		p2.getCategories().add(cat1);
 		p3.getCategories().add(cat3);
 		p4.getCategories().add(cat3);
 		p5.getCategories().add(cat2);
-		
-		productRepository.saveAll(Arrays.asList(p1,p2,p3,p4,p5));
+
+		OrderItem oi1 = new OrderItem(o1, p1, 2, p1.getPrice());
+		OrderItem oi2 = new OrderItem(o1, p3, 1, p3.getPrice());
+		OrderItem oi3 = new OrderItem(o2, p3, 2, p3.getPrice());
+		OrderItem oi4 = new OrderItem(o3, p5, 2, p5.getPrice());
+
+		productRepository.saveAll(Arrays.asList(p1, p2, p3, p4, p5));
+		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 
 	}
 
